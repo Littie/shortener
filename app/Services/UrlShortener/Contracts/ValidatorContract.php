e223a1889f10db6fdfabc8b71569364ecd7b9a29
a.polikarpov <polik.palam@gmail.com>
2019-02-23 11:21:55 +0200

    Minor refact

--- app/Services/UrlShortener/Contracts/ValidatorContract.php
+++ app/Services/UrlShortener/Contracts/ValidatorContract.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 namespace App\Services\UrlShortener\Contracts;
 
 /**
- * Class ValidatorContract
+ * Class ValidatorContract.
  * @package App\Services\UrlShortener\Contracts
  */
 interface ValidatorContract
